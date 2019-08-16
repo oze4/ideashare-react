76b2291236bd1990edd5bdeeb59036094ca978b9
Kittichote Kamalapirat <shane@Kittichotes-MacBook-Pro.local>
2019-08-16 17:14:59 +0900

    fix ecosystemm file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    apps : [{
+  apps: [
+    {
       name: 'IDEATOSHARE',
       script: 'server.js',
-  
+
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-fil$
       args: 'one two',
       instances: 1,
@@ -13,19 +14,21 @@ module.exports = {
         NODE_ENV: 'development'
       },
       env_production: {
-          PORT: 80,
+        PORT: 80,
         NODE_ENV: 'production'
       }
-    }],
-  
-    deploy : {
-      production : {
-        user : 'node',
-        host : '212.83.163.1',
-        ref  : 'origin/master',
-        repo : 'git@github.com:repo.git',
-        path : '/var/www/production',
-        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env prod$
-      }
     }
-  };
+  ],
+
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
